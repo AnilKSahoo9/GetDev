a8e0119b37beaf96dd31dcda22895a48476f52d4
sweta222 <swetastuti2@gmail.com>
2020-08-26 21:41:10 +0530

    added routing e.g path for routing in the header as well as routing in login/signup navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+//import NavBarHeader from './components/header/nav';
+import About from './components/header/about';
+import Contact from './components/header/contact';
+import Home from './components/header/home';
+import Team from './components/header/team';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBarHeader from './components/header/NavBarHeader';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBarHeader />
+        <Switch>
+          <Route path="/home" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/team" component={Team} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
