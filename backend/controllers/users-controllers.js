becdfd877ff30798be99ddf9e1746d61ae5066e4
imbhabani222 <imbhabani222@gmail.com>
2020-11-26 20:49:20 +0530

    database update commit

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -93,7 +93,7 @@ const login = async(req, res,next) => {
     }
     catch(err){
         const error=new HttpError(
-            'logging  in failed, please try again later.',
+            'logging  in failed, please try again .',
             202
         );
         return next(error); 
