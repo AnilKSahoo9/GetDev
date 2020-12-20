7e6c91f77371653346c9605befb76ae0ae14a965
sweta222 <swetastuti2@gmail.com>
2020-12-20 19:50:47 +0530

    added code to integrate search api,displaying results and their related stylings with pagination

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,414 +1,174 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
-//import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
-
+import { Button, Card, Image } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
+import PageNavigation from '../../components/PageNavigation';
+import { Form } from 'semantic-ui-react';
+import Swal from "sweetalert2";
 const Search = () => {
-  const classes = useStyles();
   const [query,setQuery] = useState('');
-  const [responseData,setResponseData] = useState([]);
-
+  const [apiData,setApiData] = useState([]);
+  const [results,setResults] = useState({});
+  const [loading,setLoading] = useState(false);
+  const [msg,setMsg] = useState('');
+  const [totalResults,setTotalResults] = useState(0);
+  const [totalPages,setTotalPages] = useState(0);
+  const [currentPageNo,setCurrentPageNo] = useState(0);
+const [Total,setTotal] = useState(0);
 const handleChange = (e) => {
 setQuery(e.target.value);
 }
+
 //console.log(query);
   const handleSubmit = (e) => {
     e.preventDefault();
-     axios.get(`http://localhost:4000/api/users/search/skill/${query}`).then(responseData => console.log(responseData))
+    if(query){
+    retrieveDataFromServer(1,query);}
+    else{
+      alert('plz input a skill');
+    }
   }
-  // ,{
-  //   params: {
-  //     ID: 12345
-  //   }
-  // }
+  //console.log(apiData)
+  const retrieveDataFromServer = (updatedPageNo = '',query) => {
+    setLoading(true);
+    const pageNumber = updatedPageNo ? `?page=${updatedPageNo}&limit=3` : '';
+    axios.get(`http://localhost:4000/api/users/search/skill/${query}${pageNumber}`).then(responseData => {console.log(responseData);
+    const resultNotFoundMsg = ! responseData.data.user.length ? 'there re no more search results.plz try a new search' : '';
+    setApiData(responseData.data.user);
+    setTotal(responseData.data.user.length);
+    const totalPagesCount = getPageCount(Total,5);
+    //setResults(responseData.data.user);
+    //setMsg(resultNotFoundMsg);
+    setTotalResults(Total);
+    setTotalPages(totalPagesCount);
+    setCurrentPageNo(updatedPageNo);
+    setLoading(false);
+  })
+  }
+
+  const getPageCount = (total,denominator) => {
+    const divisible = 0 === total % denominator;
+    const valueToBeAdded = divisible ? 0 : 1;
+    return Math.floor(total / denominator) + valueToBeAdded;
+  }
+
+  const handlePageClick = (type,Event) => {
+      //Event.preventDefault();
+      const updatePageNo = 'prev' === type ? currentPageNo - 1 : currentPageNo + 1;
+      if(!loading){
+        setLoading(true);
+        retrieveDataFromServer(updatePageNo,query);
+      }
+
+  }
+
+  const showPrevLink = 1 < currentPageNo;
+  const showNextLink = totalPages > currentPageNo;
+//  const renderSearchResults = () => {
+//   if(Object.keys(results).length && results.length){
+//     return(
+//       <div className="results-container">
+//         {results.map((item) => {
+//           return(
+//             <a key={item.id} href={item.github} className="result-item">
+//               <h6 className="image-username">{item.name}</h6>
+//               <div className="image-wrapper">
+//                 <img className="image" src={item.image} alt={`${item.name} image`}/>
+//               </div>
+//             </a>
+//           )
+//         })}
+//       </div>
+//     );
+//   }
+//  };
+
+console.log(apiData);
+//console.log(apiData.length);
+//console.log(results);
+
+const giveAlert = () => {
+  return(
+    <div>
+      {Swal.fire({
+            icon: "success",
+            title: "",
+            text: "User requested successfully",
+            showConfirmButton: false,
+            timer: 2000,
+          })}
+    </div>
+  );
+}
 return(
   <div>
-<div className="searchForm">
-  <form onSubmit={handleSubmit}>
-    <input type="text" id="filter" placeholder="Search for..." onChange={handleChange}/>
-    <input type="submit" value="Submit" />
-  </form>
-</div>
-<div className="cards">
-<Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
+  {/* <div className="searchForm">
+    <form onSubmit={handleSubmit}>
+      <input type="text" id="filter" placeholder="Search for..." onChange={handleChange} />
+      <input type="submit" value="Submit"/>
+    </form>
+  </div> */}
+  <div className="searchForm">
+  <Form onSubmit={handleSubmit}>
+    <Form.Group>
+          <Form.Input
+            placeholder='Search....'
+            name="name"
+            onChange={handleChange}
+          />
+          <Form.Button content='Search' />
+    </Form.Group>
+      </Form>
+  </div>
+  
+  <div className="cards2">
+    {apiData.length >= 1  ? (
+      //<div><h1>success</h1></div>
+      <div>
+
+      <div className="cards">
+        {apiData.map((item) => (<ol key={item.id}>
+        <Card.Group>
+    <Card>
+      <Card.Content>
+        <Card.Header>{item.name}</Card.Header>
+        <Card.Meta>{item.email}</Card.Meta>
+        <Card.Description>
+          <strong>{item.skill.join(",")}</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green' onClick={giveAlert}>
+            request
+          </Button>
+        </div>
+      </Card.Content>
     </Card>
-</div>
-</div>
+    </Card.Group>
+      </ol>))}
+     </div>
+
+     <div className="linkcontainer">
+      <PageNavigation 
+      loading={loading}
+      showPrevLink={showPrevLink}
+      showNextLink={showNextLink}
+      handlePrevClick={() => handlePageClick('prev',Event)}
+      handleNextClick={() => handlePageClick('next',Event)}
+      />
+
+      </div>
+
+      </div>
+    ) : console.log("failed")}
+
+    </div>
+
+  </div>
+  //{renderSearchResults}
 );
 }
 export default Search;
-// const Search = () => {
-//   const [countries, setCountries] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [search, setSearch] = useState("");
-//   const [filteredCountries, setFilteredCountries] = useState([]);
 
-//   useEffect(() => {
-//     setLoading(true);
-//     axios
-//       .get("https://restcountries.eu/rest/v2/all")
-//       .then((res) => {
-//         setCountries(res.data);
-//         setLoading(false);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }, []);
-
-//   useEffect(() => {
-//     setFilteredCountries(
-//       countries.filter((country) =>
-//         country.name.toLowerCase().includes(search.toLowerCase())
-//       )
-//     );
-//   }, [search, countries]);
-
-//   if (loading) {
-//     return <p>Loading countries...</p>;
-//   }
-
-//   return (
-//     <div className="search-container">
-//       <h1>Countries Lists</h1>
-//         <a
-//           style={{ color: "red" }}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           href="https://www.youtube.com/channel/UCdItDI6oTgPW7l9WOJI7ItA/"
-//         >
-//           D'coders youtube channel
-//         </a>
-
-//       <input
-//         type="text"
-//         placeholder="Search Countries"
-//         onChange={(e) => setSearch(e.target.value)}
-//       />
-//       {filteredCountries.map((country, idx) => (
-//         <CountryDetail key={idx} {...country} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// const CountryDetail = (props) => {
-//   const { name, flag } = props;
-
-//   return (
-//     <>
-//       <p>
-//         <img src={flag} alt={name} style={{ width: "20px", height: "20px" }} />
-//       </p>
-//       <p>{name}</p>
-//     </>
-//   );
-// };
-// export default Search;
-
-
-
-// import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-// import moment from 'moment';
-// import {
-//   ErrorBoundary,
-//   Facet,
-//   SearchProvider,
-//   SearchBox,
-//   Results,
-//   PagingInfo,
-//   ResultsPerPage,
-//   Paging,
-//   Sorting,
-//   WithSearch,
-// } from "@elastic/react-search-ui";
-// import {
-//   BooleanFacet,
-//   Layout,
-//   SingleSelectFacet,
-//   SingleLinksFacet,
-// } from "@elastic/react-search-ui-views";
-// import "@elastic/react-search-ui-views/lib/styles/styles.css";
-
-// const SORT_OPTIONS = [
-//   {
-//     name: "Relevance",
-//     value: "",
-//     direction: "",
-//   },
-//   {
-//     name: "Title",
-//     value: "title",
-//     direction: "asc",
-//   },
-// ];
-
-// const connector = new AppSearchAPIConnector({
-//   searchKey: "search-371auk61r2bwqtdzocdgutmg",
-//   engineName: "search-ui-examples",
-//   hostIdentifier: "host-2376rb",
-//   endpointBase: "",
-// });
-
-// const config = {
-//   alwaysSearchOnInitialLoad: true,
-//   searchQuery: {
-//     result_fields: {
-//       title: {
-//         snippet: {
-//           size: 100,
-//           fallback: true,
-//         },
-//       },
-//       nps_link: {
-//         raw: {},
-//       },
-//       description: {
-//         snippet: {
-//           size: 100,
-//           fallback: true,
-//         },
-//       },
-//     },
-//     disjunctiveFacets: ["acres", "states"],
-//     facets: {
-//       world_heritage_site: { type: "value" },
-//       states: { type: "value", size: 30 },
-//       acres: {
-//         type: "range",
-//         ranges: [
-//           { from: -1, name: "Any" },
-//           { from: 0, to: 1000, name: "Small" },
-//           { from: 1001, to: 100000, name: "Medium" },
-//           { from: 100001, name: "Large" },
-//         ],
-//       },
-//       location: {
-//         // San Francisco. In the future, make this the user's current position
-//         center: "37.7749, -122.4194",
-//         type: "range",
-//         unit: "mi",
-//         ranges: [
-//           { from: 0, to: 100, name: "Nearby" },
-//           { from: 100, to: 500, name: "A longer drive" },
-//           { from: 500, name: "Perhaps fly?" }
-//         ]
-//       },
-//       date_established: {
-//         type: "range",
-
-//         ranges: [
-//           {
-//             from: moment().subtract(50, "years").toISOString(),
-//             name: "Within the last 50 years"
-//           },
-//           {
-//             from: moment().subtract(100, "years").toISOString(),
-//             to: moment().subtract(50, "years").toISOString(),
-//             name: "50 - 100 years ago"
-//           },
-//           {
-//             to: moment().subtract(100, "years").toISOString(),
-//             name: "More than 100 years ago"
-//           }
-//         ]
-//       },
-//       visitors: {
-//         type: "range",
-//         ranges: [
-//           { from: 0, to: 10000, name: "0 - 10000" },
-//           { from: 10001, to: 100000, name: "10001 - 100000" },
-//           { from: 100001, to: 500000, name: "100001 - 500000" },
-//           { from: 500001, to: 1000000, name: "500001 - 1000000" },
-//           { from: 1000001, to: 5000000, name: "1000001 - 5000000" },
-//           { from: 5000001, to: 10000000, name: "5000001 - 10000000" },
-//           { from: 10000001, name: "10000001+" }
-//         ]
-//       }
-//     },
-//   },
-//   autocompleteQuery: {
-//     results: {
-//       resultsPerPage: 5,
-//       result_fields: {
-//         title: {
-//           snippet: {
-//             size: 100,
-//             fallback: true,
-//           },
-//         },
-//         nps_link: {
-//           raw: {},
-//         },
-//       },
-//     },
-//     suggestions: {
-//       types: {
-//         documents: {
-//           fields: ["title", "description"],
-//         },
-//       },
-//       size: 4,
-//     },
-//   },
-//   apiConnector: connector,
-// };
-
-// export default function App() {
-//   return (
-//     <SearchProvider config={config}>
-//       <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-//         {({ wasSearched }) => {
-//           return (
-//             <div className="App">
-//               <ErrorBoundary>
-//                 <Layout
-//                   header={
-//                     <SearchBox
-//                       autocompleteMinimumCharacters={3}
-//                       //searchAsYouType={true}
-//                       autocompleteResults={{
-//                         linkTarget: "_blank",
-//                         sectionTitle: "Results",
-//                         titleField: "title",
-//                         urlField: "nps_link",
-//                         shouldTrackClickThrough: true,
-//                         clickThroughTags: ["test"],
-//                       }}
-//                       autocompleteSuggestions={true}
-//                       debounceLength={0}
-//                     />
-//                   }
-//                   sideContent={
-//                     <div>
-//                       {wasSearched && (
-//                         <Sorting label={"Sort by"} sortOptions={SORT_OPTIONS} />
-//                       )}
-//                       <Facet
-//                         field="states"
-//                         label="States"
-//                         filterType="any"
-//                         isFilterable={true}
-//                       />
-//                       {/* <Facet
-//                         field="world_heritage_site"
-//                         label="World Heritage Site?"
-//                         view={BooleanFacet}
-//                       />
-//                       <Facet
-//                         field="visitors"
-//                         label="Visitors"
-//                         view={SingleLinksFacet}
-//                       />
-//                       <Facet
-//                         field="date_established"
-//                         label="Date Established"
-//                         filterType="any"
-//                       />
-//                       <Facet
-//                         field="location"
-//                         label="Distance"
-//                         filterType="any"
-//                       /> */}
-//                       <Facet
-//                         field="acres"
-//                         label="Acres"
-//                         view={SingleSelectFacet}
-//                       />
-//                     </div>
-//                   }
-//                   bodyContent={
-//                     <Results
-//                       titleField="title"
-//                       urlField="nps_link"
-//                       shouldTrackClickThrough={true}
-//                     />
-//                   }
-//                   bodyHeader={
-//                     <React.Fragment>
-//                       {wasSearched && <PagingInfo />}
-//                       {wasSearched && <ResultsPerPage />}
-//                     </React.Fragment>
-//                   }
-//                   bodyFooter={<Paging />}
-//                 />
-//               </ErrorBoundary>
-//             </div>
-//           );
-//         }}
-//       </WithSearch>
-//     </SearchProvider>
-//   );
-// }
-
-
-
-{
-  /* <nav>
-        <div class="nav-wrapper">
-          <form>
-            <div class="input-field">
-              <input id="search" type="search" required />
-              <label for="search">
-                <i class="material-icons">search</i>
-              </label>
-              <i class="material-icons">close</i>
-            </div>
-          </form>
-        </div>
-      </nav> */
-  // <div className="search_container">
-  //   <h2>live search here</h2>
-  //   <label className="search-label" htmlFor="search-input">
-  //     <input
-  //       type="text"
-  //       value=""
-  //       name="query"
-  //       id="search-input"
-  //       placeholder="Search"
-  //       onChange={handleChanges} //{(event) => setQuery(event.target.value)}
-  //     />
-  //   </label>
-  // </div>
-}
